8036ff33b87fc40949f7df6d4017c65b33dc20dc
archangel2006 <vaibhavideep26@gmail.com>
2025-10-21 20:29:02 +0530

    chore: setup basic Next.js structure
    Added test pages and components to visualize routing and layout connections (page.jsx, layout.js).

---
+++ src/app/blog/[id]/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogLayout = ({children}) => {
+  return (
+    <div>
+      <h1>Hi This Is Blog !</h1>
+      {children}
+    </div>
+  )
+}
+export default BlogLayout
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
+        <h1>Main Layout</h1>
         {children}
+        <Footer />
       </body>
     </html>
   );
 }
+     
